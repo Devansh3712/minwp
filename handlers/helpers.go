ab4a580d810f3b4ca8a563601bffefdd40c01bec
Devansh3712 <devanshamity@gmail.com>
2023-03-12 20:20:19 +0530

    Update CORS

--- handlers/helpers.go
+++ handlers/helpers.go
@@ -27,6 +27,7 @@ func notFound() http.HandlerFunc {
 
 func enableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
 }
 
 func get(f http.HandlerFunc) http.HandlerFunc  { return allowMethod(f, http.MethodGet) }
